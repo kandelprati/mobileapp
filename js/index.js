024c942924887479d0b210d8e8b1bd98353a21ff
kandel Pratima <s247034@gs.sundaidenshi.ac.jp>
2025-10-21 16:31:32 +0900

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-window.addEventListener("DOMContentLoaded", function () {
-  const items = document.querySelectorAll(".item");
-  items.forEach(function (element, index) {
-    setTimeout(function () {
-      element.classList.add("fade-in");
-    }, 200 * index);
-  });
-});
